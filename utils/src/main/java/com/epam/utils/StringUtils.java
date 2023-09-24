12fbe0e661422dd3b44c519070ed715c61824499
JustDevWeb <zm91672@gmail.com>
2023-09-24 14:10:08 +0500

    fix is positive method

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -2,14 +2,14 @@ package com.epam.utils;
 
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-        if (str == null || str.isEmpty()) {
-            return false;
-        }
-
-        try {
-            int number = Integer.parseInt(str);
-            return number > 0;
-        } catch (NumberFormatException e) {
+        if (str != null && !str.isEmpty()) {
+            try {
+                int number = Integer.parseInt(str);
+                return number > 0;
+            } catch (NumberFormatException var2) {
+                return false;
+            }
+        } else {
             return false;
         }
     }
