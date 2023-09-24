234d222857d3101048f2641471601059537684df
JustDevWeb <zm91672@gmail.com>
2023-09-24 15:01:39 +0500

    use lang3 library in isPositiveNumber

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -1,13 +1,10 @@
 package com.epam.utils;
+import org.apache.commons.lang3.math.NumberUtils;
 
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-            try {
-                int number = Integer.parseInt(str);
-                return number > 0;
-            } catch (NumberFormatException e) {
-                return false;
-            }
-        }
+        return NumberUtils.toInt(str) != 0 && NumberUtils.toInt(str) > 0;
+    }
+
     }
 
