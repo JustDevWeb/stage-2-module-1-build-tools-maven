3cef8fef2212c076a44efc73a3eee44174c12814
JustDevWeb <zm91672@gmail.com>
2023-09-24 14:35:31 +0500

    fix isPositiveNumber

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -7,16 +7,12 @@ import static com.epam.utils.StringUtils.isPositiveNumber;
 
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
-        if ( args.size() > 0 ){
             for (String arg : args) {
                 if ( !isPositiveNumber(arg) ) {
                     return false;
                 }
             }
             return true;
-        } else {
-            return false;
-        }
     }
 
 }
--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -2,15 +2,12 @@ package com.epam.utils;
 
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-        if (str != null && !str.isEmpty()) {
             try {
                 int number = Integer.parseInt(str);
                 return number > 0;
-            } catch (NumberFormatException var2) {
+            } catch (NumberFormatException e) {
                 return false;
             }
-        } else {
-            return false;
         }
     }
-}
+
