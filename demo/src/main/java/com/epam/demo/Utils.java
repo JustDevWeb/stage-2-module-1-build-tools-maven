2f28447a61373b52f87cd81faf61f9f08417ddba
JustDevWeb <zm91672@gmail.com>
2023-09-24 13:44:00 +0500

    fix test error

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -9,10 +9,9 @@ public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
        for (String arg : args) {
            if ( !isPositiveNumber(arg) ) {
-               break;
+               return false;
            }
        }
-
        return true;
     }
 }
--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -2,7 +2,13 @@ package com.epam.utils;
 
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-        int number = Integer.parseInt(str);
-        return number > 0;
+
+        try {
+            int number = Integer.parseInt(str);
+            return number > 0;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+
     }
 }
