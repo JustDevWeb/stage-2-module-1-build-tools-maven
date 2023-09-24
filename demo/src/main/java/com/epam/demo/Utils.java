3babf7f2a95a28781b42940ee319a7a77d33747e
JustDevWeb <zm91672@gmail.com>
2023-09-24 14:17:59 +0500

    fix isAllPositiveNumbers method

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -7,11 +7,16 @@ import static com.epam.utils.StringUtils.isPositiveNumber;
 
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
-       for (String arg : args) {
-           if ( !isPositiveNumber(arg) ) {
-               return false;
-           }
-       }
-       return true;
+        if ( args.size() > 0 ){
+            for (String arg : args) {
+                if ( !isPositiveNumber(arg) ) {
+                    return false;
+                }
+            }
+            return true;
+        } else {
+            return false;
+        }
     }
+
 }
